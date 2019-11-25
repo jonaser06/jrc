6e9a3b73a702373e2cff72ff1f0ff350d2c7dc86
Jonathan Narvaez <jonaser06@gmail.com>
2019-11-24 19:22:27 -0500

    avance

--- view/modules/menu.php
+++ view/modules/menu.php
@@ -14,7 +14,7 @@
           <img src="<?php echo $dir; ?>dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
-          <p>Alexander Pierce</p>
+          <p><?php echo $_SESSION['nombres']; ?></p>
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>
